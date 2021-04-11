f3ad8d94f3416c9a82575f80ac244445e4509ebd
오예진 <oyejin@oyejin-ui-MacBookPro.local>
2021-04-11 12:04:15 +0900

    [ADD] 비밀번호와 아이디를 입력안했을 경우

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-
-
 var counter = 1;
 setInterval(function(){
     document.getElementById('radio'+counter).checked = true;
@@ -9,3 +7,25 @@ setInterval(function(){
     }
 }, 5000);
 
+function login(){
+    /* 로그인 폼 - 아이디 혹은 비밀번호를 입력 안했을 경우 */
+    var loginForm= document.loginForm;
+    var id = loginForm.id.value;
+    var pw = loginForm.pw.value;
+
+    if(!id && !pw){
+        alert("아이디와 비밀번호를 모두 입력해주세요.");
+        return false;
+    }
+    else if(!id){
+        alert("아이디를 입력해주세요");
+        return false;
+    }
+    else if(!pw){
+        alert("비밀번호를 입력해주세요");
+        return false;
+    }
+    else{
+        return true;
+    }
+}
